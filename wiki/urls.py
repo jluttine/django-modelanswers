f0d1bef15f1aa81e952985a581842301a042db91
Jaakko Luttinen <jaakko.luttinen@iki.fi>
2012-03-28 02:23:45 +0300

    Added very preliminary functionality for adding new exercises, listing all exercises and deleting them.

--- wiki/urls.py
+++ wiki/urls.py
@@ -1,7 +1,10 @@
 from django.conf.urls.defaults import patterns, include, url
 
 urlpatterns = patterns('wiki.views',
+    url(r'^exercises/$', 'list_exercises'),
+    url(r'^exercises/add/$', 'add_exercise'),
     url(r'^exercises/(?P<exercise_id>\w+)/$', 'show_exercise'),
     url(r'^exercises/(?P<exercise_id>\w+)/edit/$', 'edit_exercise'),
+    url(r'^exercises/(?P<exercise_id>\w+)/delete/$', 'delete_exercise'),
     
 )
--- wiki/views.py
+++ wiki/views.py
@@ -5,7 +5,12 @@ from django.core.urlresolvers import reverse
 from django.conf import settings
 from wiki.models import Exercise
 from wiki.forms import ExerciseForm
-from django.conf import settings
+
+#from django import forms
+from django.contrib.auth.forms import UserCreationForm
+#from django.http import HttpResponseRedirect
+#from django.shortcuts import render_to_response
+
 
 # Some conventions:
 # http://ericholscher.com/projects/django-conventions/app/
@@ -14,6 +19,7 @@ from django.conf import settings
 MATHJAX_URL = getattr(settings,
                       'MATHJAX_URL',
                       'http://cdn.mathjax.org/mathjax/latest/MathJax.js?config=TeX-AMS-MML_HTMLorMML')
+
 
 def show_exercise(request,
                   exercise_id,
@@ -24,15 +30,16 @@ def show_exercise(request,
         raise Http404
     return render(request,
                   template_name,
-                  {'exercise': exercise,
-                   'MATHJAX_URL': MATHJAX_URL})
+                  {
+                      'exercise': exercise,
+                      'MATHJAX_URL': MATHJAX_URL,
+                  })
 
 @login_required
 def edit_exercise(request,
                   exercise_id,
                   form_class=ExerciseForm,
                   template_name='wiki/edit_exercise.html'):
-    print(request.user)
     # Find the exercise
     try:
         exercise = Exercise.objects.get(pk=exercise_id)
@@ -50,6 +57,59 @@ def edit_exercise(request,
         form = form_class(instance=exercise)
     return render(request,
                   template_name,
-                  {'form': form})
+                  {
+                      'form': form,
+                  })
 
 
+@login_required
+def delete_exercise(request,
+                  exercise_id,
+                  template_name='wiki/show_exercise.html'):
+
+    try:
+        exercise = Exercise.objects.get(pk=exercise_id)
+    except Exercise.DoesNotExist:
+        raise Http404
+    return render(request,
+                  template_name,
+                  {
+                      'exercise': exercise,
+                      'MATHJAX_URL': MATHJAX_URL,
+                  })
+
+
+@login_required
+def add_exercise(request,
+                 form_class=ExerciseForm,
+                 template_name='wiki/edit_exercise.html'):
+
+    if request.method == 'POST':
+        # Handle posted edit
+        form = form_class(request.POST)
+        if form.is_valid():
+            exercise_id = form.cleaned_data['id']
+            print('id', exercise_id)
+            form.save()
+            return HttpResponseRedirect(reverse('wiki.views.show_exercise', 
+                                                args=(exercise_id,)))
+    else:
+        # Initialize edit
+        form = form_class()
+        
+    return render(request,
+                  template_name,
+                  {
+                      'form': form,
+                  })
+
+def list_exercises(request,
+                   template_name='wiki/list_exercises.html'):
+
+    exercise_list = Exercise.objects.all()
+    
+    return render(request,
+                  template_name,
+                  {
+                      'exercise_list': exercise_list,
+                  })
